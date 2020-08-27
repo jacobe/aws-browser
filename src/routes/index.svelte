cbd064e36dc8f7be37004b16f1ff05ae86556c3d
Jacob Eisenberg <jacobe@github.com>
2020-08-27 11:41:48 +0200

    Add starter template

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<style>
+</style>
+
+<svelte:head>
+	<title>EC2 browser</title>
+</svelte:head>
+
+<h1>EC2 instances</h1>
+<table>
+	<thead>
+		
+	</thead>
+	<tbody>
+		<tr>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
